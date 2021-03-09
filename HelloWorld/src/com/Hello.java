0c2b671980e68f8caed602c60709f0e8c45a19d7
lch <z782346232@163.com>
2021-03-09 11:29:37 +0800

    helloworld

---
+++ HelloWorld/src/com/Hello.java
@@ -0,0 +1,17 @@
+package com;
+
+public class Hello {
+    public  int a=45;
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+ System.out.println("hello world!"); 
+System.out.println("hello world!");
+System.out.println("hello world!");
+System.out.println("hello world!");
+System.out.println("hello world!");
+System.out.println("hello world!");
+System.out.println("hello world!");
+
+    }
+
+}
